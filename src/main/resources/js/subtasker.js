265671b40009259a32210d1e71e2e8dd3e149bad
jeanarcand <jarcand@gmail.com>
2013-05-08 22:04:24 -0400

    showing an empty popup on a web-item click.

--- src/main/resources/js/subtasker.js
+++ src/main/resources/js/subtasker.js
@@ -1,1 +1,8 @@
-alert('it works');
+function showthat() {
+	var popup = new AJS.Dialog(860, 530);
+	popup.show();
+}
+
+AJS.$(document).ready(function() {
+	AJS.$("#subtasker-web-item-link").attr("href", "javascript:showthat();");
+});
